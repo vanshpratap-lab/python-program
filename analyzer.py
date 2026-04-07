94d6608a88ee9d1391d2ed57fd354d57a88eb5ec
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-04-07 23:45:52 +0530

    Added sorting and slicing

--- analyzer.py
+++ analyzer.py
@@ -18,4 +18,8 @@ print(text)
 most_common = max(word_count, key = word_count.get)
 print(f"most common : {most_common}")
 print(f"word count : {word_count[most_common]}")
-    
+top_n = int(input("how many top words do you want to see "))
+sorted_words = sorted(word_count, key = word_count.get, reverse = True)
+print(f"top {top_n} words:")
+for word in sorted_words[:top_n]:
+    print(f"{word} : {word_count[word]}")
