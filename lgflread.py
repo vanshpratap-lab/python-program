f84886fef335f0fb219838484d0aaea80fb0a622
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-04-10 23:50:06 +0530

    upgraded log file reader

--- lgflread.py
+++ lgflread.py
@@ -8,7 +8,7 @@ while True:
     elif user_input == "reset":
         f.seek(0)
         print("reset to beginning")
-    else:
+    elif user_input == "read":
         line = f.readline()
 
         if not line:
@@ -16,3 +16,12 @@ while True:
             break
         print(f"log : {line}")
         print(f"position : {f.tell()}")
+    elif user_input == "jump":
+        pos = int(input("please enter your position: "))
+        print(f"jumped to : {f.tell()}")
+
+    elif user_input == "pos":
+        print(f"current position : {f.tell()}")
+    else:
+        print("invalid command")
+ 
