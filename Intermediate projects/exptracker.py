06c64b7d74b0d2cdf8d3746dbc999c385ae1ad75
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-04-15 23:32:05 +0530

    updated expense tracker

--- Intermediate projects/exptracker.py
+++ Intermediate projects/exptracker.py
@@ -1,9 +1,11 @@
+from functools import reduce
 types = input("pleaase enter your type : ")
 cost = int(input("please enter your cost : "))
 
 with open('expense.txt', 'a') as f:
     f.write(f"{types};{cost}\n")
-
+cost_list = []
+expense_list = []
 with open('expense.txt', 'r') as g:
     lines = g.readlines()
     for line in lines :
@@ -12,4 +14,15 @@ with open('expense.txt', 'r') as g:
         types = parts[0]
         cost = parts[1]
         print(f"{types}:{cost}")
+        cost_list.append(int(cost))
+        expense_list.append((types,int(cost)))
 
+if cost_list:
+    total = reduce(lambda a,b : a + b, cost_list )
+else:
+    total = 0
+print(f"total : {total}")
+expense = filter(lambda x : x[1] > 450, expense_list)
+filtered_expense = list(expense)
+for item in filtered_expense :
+    print(f"{item[0]} : {item[1]}")
