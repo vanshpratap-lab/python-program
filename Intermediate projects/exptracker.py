4f2672c72760f1c58723790750e3cf2a9cdae657
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-04-16 23:35:36 +0530

    updated expense tracker

--- Intermediate projects/exptracker.py
+++ Intermediate projects/exptracker.py
@@ -1,28 +1,48 @@
 from functools import reduce
-types = input("pleaase enter your type : ")
-cost = int(input("please enter your cost : "))
+while True:
+    print("1. add expense")
+    print("2. view expense")
+    print("3. show total expense")
+    print("4. filter expense")
+    print("5. exit")
 
-with open('expense.txt', 'a') as f:
-    f.write(f"{types};{cost}\n")
-cost_list = []
-expense_list = []
-with open('expense.txt', 'r') as g:
-    lines = g.readlines()
-    for line in lines :
-        line = line.strip()
-        parts = line.split(';')
-        types = parts[0]
-        cost = parts[1]
-        print(f"{types}:{cost}")
-        cost_list.append(int(cost))
-        expense_list.append((types,int(cost)))
+    user_input = int(input("please enter your choice"))
 
-if cost_list:
-    total = reduce(lambda a,b : a + b, cost_list )
-else:
-    total = 0
-print(f"total : {total}")
-expense = filter(lambda x : x[1] > 450, expense_list)
-filtered_expense = list(expense)
-for item in filtered_expense :
-    print(f"{item[0]} : {item[1]}")
+    if user_input == "5":
+        print("thanks have a good day")
+        break
+
+    elif user_input == "1":
+        print("add expense")
+        types = input("pleaase enter your type : ")
+        cost = int(input("please enter your cost : "))
+        with open('expense.txt', 'a') as f:
+            f.write(f"{types};{cost}\n")
+        
+        cost_list = []
+        expense_list = []
+
+    elif user_input == "2":
+        with open('expense.txt', 'r') as g:
+            lines = g.readlines()
+            line = line.strip()
+            parts = line.split(';')
+            types = [0]
+            cost = [1]
+            print(f"{types}:{cost}")
+            cost_list.append(int(cost))
+            expense_list.append((types,int(cost)))
+        
+
+    elif user_input == "3":
+        if cost_list :
+            total = reduce(lambda c,d : c + d , cost_list)
+        else:
+            total = 0
+        print(f"total : {total}")
+
+    elif user_input == "4":
+        expense = filter(lambda x : x[1] > 450, expense_list)
+        filtered_expense = list(expense)
+        for item in filtered_expense :
+            print(f"{item[0]} : {item[1]}")
