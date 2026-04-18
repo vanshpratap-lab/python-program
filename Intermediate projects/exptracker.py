48d41c692e4f6a2ac112e8ec2850f854c38fec0d
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-04-18 12:22:56 +0530

    updated expense tracker

--- Intermediate projects/exptracker.py
+++ Intermediate projects/exptracker.py
@@ -1,4 +1,6 @@
 from functools import reduce
+cost_list = []
+expense_list = []
 while True:
     print("1. add expense")
     print("2. view expense")
@@ -6,7 +8,7 @@ while True:
     print("4. filter expense")
     print("5. exit")
 
-    user_input = int(input("please enter your choice"))
+    user_input = input("please enter your choice")
 
     if user_input == "5":
         print("thanks have a good day")
@@ -19,19 +21,24 @@ while True:
         with open('expense.txt', 'a') as f:
             f.write(f"{types};{cost}\n")
         
-        cost_list = []
-        expense_list = []
 
     elif user_input == "2":
         with open('expense.txt', 'r') as g:
             lines = g.readlines()
-            line = line.strip()
-            parts = line.split(';')
-            types = [0]
-            cost = [1]
-            print(f"{types}:{cost}")
-            cost_list.append(int(cost))
-            expense_list.append((types,int(cost)))
+
+            for line in lines:
+                line = line.strip()
+                if not line :
+                    continue
+
+
+
+                parts = line.split(';')
+                types = parts[0]
+                cost = int(parts[1])
+                print(f"{types}:{cost}")
+                cost_list.append(int(cost))
+                expense_list.append((types,int(cost)))
         
 
     elif user_input == "3":
@@ -46,3 +53,5 @@ while True:
         filtered_expense = list(expense)
         for item in filtered_expense :
             print(f"{item[0]} : {item[1]}")
+    else:
+        print("inavlid input")
